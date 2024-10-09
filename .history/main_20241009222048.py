51174cb14a9d6e7f9241365c42bff305943e6845
Ramamoorthy Luxman <l.ramamoorthy01@gmail.com>
2024-10-09 22:22:22 +0200

    created main.py

---
+++ .history/main_20241009222048.py
@@ -0,0 +1,12 @@
+import importlib
+from utils import params
+from utils import dataset
+import os
+import train
+import numpy as np
+
+importlib.reload(params)
+
+data = dataset(params.ACQ_PATHS)
+
+train = train.train(data.distance_matrices, data.cosine_matrices, data.surface_albedos, data.surface_normals, data.images)
---
+++ .history/main_20241009222101.py
@@ -0,0 +1,11 @@
+import importlib
+from utils import params
+from utils import dataset
+import os
+import train
+import numpy as np
+
+
+data = dataset(params.ACQ_PATHS)
+
+train = train.train(data.distance_matrices, data.cosine_matrices, data.surface_albedos, data.surface_normals, data.images)
